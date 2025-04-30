9270a7fce120bece44c60217815a05af656fa4cb
Eugene <eugenelevkovych@yahoo.com>
2025-04-30 11:15:02 +0300

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import Search from "./Search";
+
+
+export default function NavBar() {
+    
+    return  (<nav className="nav-bar">
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+   <Search />
+    <p className="num-results">
+      Found <strong>X</strong> results
+    </p>
+  </nav>)
+
+}
