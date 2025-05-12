c0f30ded8a675c78264864d36d4fc69578e70eac
Eugene <eugenelevkovych@yahoo.com>
2025-05-12 13:37:36 +0300

    add actors

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ useEffect(function () {
     setIsLoading(true);
     setError("")
     
-    const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
 
     if(!res.ok) throw new Error("Something went wrong with fetching movies");
 
